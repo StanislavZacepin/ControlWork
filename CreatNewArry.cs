94621348a7e0a850e9728508c23b145fc77feac9
†¤¤† †¤¤† <yaolsher@yandex.ru>
2022-05-08 12:03:41 +0300

    Пробегаем по масиву и делаем проверку

--- CreatNewArry.cs
+++ CreatNewArry.cs
@@ -2,10 +2,18 @@
 
 public class CreatNewArry
 {
+	string textSum = string.Empty;
 
-
-	public CreatArry()
+	public int [] CreatArry(string[] arry)
 	{
+        for (int i = 0; i < arry.Length; i++)
+        {
+            if (arry[i].Length <= 3)
+            {
+                textSum += $"{arry[i]},";
+            }
+        }          
 
-	}
+        return ;
+    }
 }
